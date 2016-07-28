7cf6d8be4b0770e0ff14caf47812933bd38f399d
silentcloud <rjmuqiang@gmail.com>
2016-07-28 13:33:02 +0800

    use babel-register

--- src/compiler.js
+++ src/compiler.js
@@ -1,38 +1,15 @@
-import fs from 'fs';
-const babel = require('babel-core');
+require('babel-register')({
+  presets: [
+    require.resolve('babel-preset-es2015'),
+    require.resolve('babel-preset-react'),
+    require.resolve('babel-preset-stage-0'),
+  ],
+  plugins: [
+    require.resolve('babel-plugin-add-module-exports'),
+  ],
+});
 
 const noop = () => null;
-
-const jsCompiler = ext => {
-  const origJs = require.extensions[ext];
-  return (module, filename) => {
-    if (filename.indexOf('node_modules/') >= 0) {
-      return (origJs || require.extensions[ext])(module, filename);
-    }
-
-    const content = fs.readFileSync(filename, 'utf-8');
-    const code = babel.transform(content, {
-      presets: [
-        require.resolve('babel-preset-es2015'),
-        require.resolve('babel-preset-react'),
-        require.resolve('babel-preset-stage-0'),
-      ],
-      plugins: [
-        require.resolve('babel-plugin-add-module-exports'),
-      ],
-      ast: false,
-    }).code;
-
-    return module._compile(code, filename);
-  }
-}
-
-require.extensions['.js'] = jsCompiler('.js');
-require.extensions['.jsx'] = jsCompiler('.jsx');
-
-require.extensions['.css'] = noop;
-require.extensions['.less'] = noop;
-require.extensions['.scss'] = noop;
-require.extensions['.sass'] = noop;
-
-require.extensions['.html'] = noop;
+['.css', '.less', '.scss', '.sass', '.html'].forEach(ext => {
+  require.extensions[ext] = noop;
+});
